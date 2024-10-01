bd6362224d74d36ba1d2ffd1450c6e2e4bbc4dbb
mikemabi <54478069+mikemabi@users.noreply.github.com>
2024-10-01 02:13:23 +0100

    Update Program.cs

--- Quiz App/Program.cs
+++ Quiz App/Program.cs
@@ -12,7 +12,7 @@ for ((int i, bool j) = (0, true); (i < 5) && j; i++)
     int num = Quiz.QuestnNum(); //call to method to get number of the quiz question the user wants
     quiz.QuestnHandler((num - 1), out char ans); //with that number he entered, selects the question and correct answer
     score = quiz.ProcessResponse((num - 1), ans, score); // call to method that compares his response to the real answer and scores    
-    j = Quiz.StopDeciscion();//call to method to know if user would like to continue
+    if (i<4) j = Quiz.StopDeciscion();//call to method to know if user would like to continue
 }
 Console.WriteLine("You are done! You scored {0} out of 5!", score);
 Console.ReadKey();
